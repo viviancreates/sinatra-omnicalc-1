b26c944a189e139b5c0d1f5e825d700367f98312
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-10 23:25:07 +0000

    Add video notes and get started

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# don't forget need to bundle install then bin/server to make port work ???is this true
+
+get("/howdy") do
+  erb(:hello)
+end 
+
+get("/goodbye") do
+  erb(:bye)
+end 
+
+get("/square/new") do
+  erb(:new_square_calc)
+end 
+
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>

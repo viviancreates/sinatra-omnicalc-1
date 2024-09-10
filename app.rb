c86ac282c1389c533e240cbdcaf9aef718a59a6b
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-10 23:50:38 +0000

    Finish notes and first section, take break

--- app.rb
+++ app.rb
@@ -15,7 +15,13 @@ get("/square/new") do
   erb(:new_square_calc)
 end 
 
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
 
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
+end 
 
 get("/") do
   "

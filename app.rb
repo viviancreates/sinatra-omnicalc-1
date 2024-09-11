47fc4e44165d98dac6f8076971596f4049c16520
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-11 00:27:49 +0000

    Add square root new and results routes and erb files

--- app.rb
+++ app.rb
@@ -16,11 +16,31 @@ get("/square/new") do
 end 
 
 get("/square/results") do
-  @the_num = params.fetch("users_number").to_f
+  @the_num = params.fetch("number").to_f
 
   @the_result = @the_num ** 2
 
   erb(:square_results)
+end 
+
+get("/square_root/new") do
+  erb(:square_root_new)
+end 
+
+get("/square_root/results") do
+  @the_num = params.fetch("user_number").to_f
+
+  @the_result = Math.sqrt(@the_num)
+
+  erb(:square_root_results)
+end 
+
+get("/random/new") do
+  erb(:random_new)
+end 
+
+get("/payment/new") do
+  erb(:payment_new)
 end 
 
 get("/") do

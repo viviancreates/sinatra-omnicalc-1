4d0b7e41d58521a3164bab6c65d52e110414a913
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-11 00:56:57 +0000

    Add payment input and calculations

--- app.rb
+++ app.rb
@@ -55,6 +55,25 @@ get("/payment/new") do
   erb(:payment_new)
 end 
 
+get("/payment/results") do
+ apr = params.fetch("user_apr").to_f / 100 / 12 
+ years = params.fetch("user_years").to_i
+ present_value = params.fetch("user_pv").to_f
+
+ total_payments = 12 *years
+
+ @numerator = present_value * apr * (1 + apr) ** total_payments
+ @denominator = (1 + apr) ** total_payments - 1
+
+ @monthly_payment = @numerator / @denominator
+
+ @apr = params.fetch("user_apr").to_f
+ @years = years
+ @present_value = present_value
+
+ erb(:payment_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
